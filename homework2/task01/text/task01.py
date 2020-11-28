10d8c4dba819e28fe88e5475d8af78debbaecf9a
ptrwn <1374688@gmail.com>
2020-11-29 05:09:37 +0700

    intermediate res

--- homework2/task01/text/task01.py
+++ homework2/task01/text/task01.py
@@ -11,38 +11,36 @@ from typing import List
 
 def get_longest_diverse_words(file_path: str) -> List[str]:
 
-
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         lines = f.readlines()
 
     # replace '\u00e4' representations with the actual symbols:
-    res = [part.encode('latin1').decode('unicode_escape') for part in lines]
+    res = [part.encode("latin1").decode("unicode_escape") for part in lines]
 
     # remove tabs, newlines, and punctuation, put all words in one list
-    words_list = [word.strip('».,()«—') for line in res for word in line.strip('\t\n').split(' ') if word]
+    words_list = [
+        word.strip("».,()«—")
+        for line in res
+        for word in line.strip("\t\n").split(" ")
+        if word
+    ]
 
     # merge parts of words separated by wrap (as in 'erschaf-' + 'fene')
     clean_words = []
     for num, el in enumerate(words_list):
 
-        if el and el.endswith('-'):
-            clean_words.append(el[:-1]+words_list[num+1])
-        elif el and words_list[num-1].endswith('-'):
+        if el and el.endswith("-"):
+            clean_words.append(el[:-1] + words_list[num + 1])
+        elif el and words_list[num - 1].endswith("-"):
             continue
-        else: el and clean_words.append(el)
-
-
+        else:
+            el and clean_words.append(el)
 
     print(clean_words)
-    words_set = set()
 
 
+get_longest_diverse_words("data.txt")
 
-
-get_longest_diverse_words('data.txt')
-
-
-    
 
 def get_rarest_char(file_path: str) -> str:
     ...
